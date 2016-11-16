77652fd151deb8137bea42b992ef0be92cddb85d
nosov-pvl <nosov-pvl@yandex.ru>
2016-11-16 21:35:14 +0300

    English language support added

--- src/test/java/jsentencedetection/JSentenceDetectionTest.java
+++ src/test/java/jsentencedetection/JSentenceDetectionTest.java
@@ -18,7 +18,7 @@ import static org.junit.Assert.assertFalse;
  */
 public class JSentenceDetectionTest {
     @Test
-    public void testBasic(){
+    public void testRussian(){
 
         Scanner s = null;
         try {
@@ -42,4 +42,30 @@ public class JSentenceDetectionTest {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void testEnglish(){
+
+        Scanner s = null;
+        try {
+            s = new Scanner(new File("src/main/resources/drones.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ArrayList<String> list = new ArrayList<String>();
+        while (s.hasNextLine()){
+            list.add(s.nextLine());
+        }
+        s.close();
+
+        JSentenceDetection detector = new JSentenceDetection("english", "GoogleNews.vectors.bin");
+        assertFalse(detector.detect(list, "Coffee may save your liver from bad binge-drinking habits: 14 percent lower chance of cancer with each cup"));
+        assertTrue(detector.detect(list, "Energy Giants Turn to Drones and Sensors in New Embrace of the Digital World"));
+        try {
+            detector.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
